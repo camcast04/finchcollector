c3234867e80eb1d124080b986b344a72692dc5c1
camcast04 <cam.cast04@gmail.com>
2024-04-22 18:14:27 -0400

    Finished Lab Part 1

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    #path for finches index (see all of them)
+    path('finches/', views.finches_index, name='index')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -3,9 +3,21 @@ from .models import Finch
 
 # Create your views here.
 
+finches = [
+  {'common_name': 'House finch', 'species': 'Haemorhous mexicanus', 'description': 'Brown with streaky white breast', 'age': 3, 'sex': 'F'},
+  {'common_name': 'House finch', 'species': 'Haemorhous mexicanus', 'description': 'Red head and throat with brown streaky breast', 'age': 2, 'sex': 'M'},
+  
+  
+]
+
 def home(request):
     return render(request, 'home.html')
 
 
 def about(request):
     return render(request, 'about.html')
+
+def finches_index(request):
+    return render(request, 'finches/index.html', {
+        'finches': finches
+    })
