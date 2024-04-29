d7aac0e0f9cdf8900db3efe5a1b02efbb4fc4885
camcast04 <cam.cast04@gmail.com>
2024-04-29 19:33:32 -0400

    modules 3/4

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Finch
 
 # Create your views here.
@@ -22,3 +23,19 @@ def finches_index(request):
     return render(request, 'finches/index.html', {
         'finches': finches
     })
+    
+def finches_detail (request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    return render(request, 'finches/detail.html', {'finch': finch})
+
+class FinchCreate(CreateView):
+    model = Finch
+    fields = '__all__'
+    
+class FinchUpdate(UpdateView):
+    model = Finch
+    fields = ['common_name', 'species', 'sex', 'description', 'age']
+    
+class FinchDelete(DeleteView):
+    model = Finch
+    success_url = '/finches'
